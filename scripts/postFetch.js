987f636bc1b245416faa213d856b6299b742c406
Ritul <157266465+Ritul-Void@users.noreply.github.com>
2025-08-31 09:38:13 +0530

    switching to root level array json structure and update fetching for the json stucture , also added more info in json structure

--- scripts/postFetch.js
+++ scripts/postFetch.js
@@ -1,7 +1,17 @@
 async function getPosts() {
-    const postResponse = await fetch("https://raw.githubusercontent.com/Ritul-Void/while-true-blog/refs/heads/main/posts/posts.json")
-    const posts = await postResponse.json()
-    console.log(posts)  
+    const response = await fetch("https://raw.githubusercontent.com/Ritul-Void/while-true-blog/refs/heads/main/posts/posts.json")
+    const allPosts = await response.json()
+    return allPosts
 }
 
-getPosts()
+async function displayPosts() {
+    const posts = await getPosts()
+    console.log(posts)
+
+    console.log(posts[0].title)
+    
+}
+
+displayPosts()
+
+
